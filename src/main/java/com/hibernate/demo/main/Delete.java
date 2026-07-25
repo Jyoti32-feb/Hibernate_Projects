70b38734065133a64442ebd66d046713d2707d3b
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-25 18:25:31 +0530

    delete using remove api

---
+++ src/main/java/com/hibernate/demo/main/Delete.java
@@ -0,0 +1,24 @@
+package com.hibernate.demo.main;
+
+import com.hibernate.demo.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Delete {
+    public static void main(String[] args) {
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx=session.beginTransaction();
+        Employee employee = session.get(Employee.class, 1);
+        if (employee != null) {
+            session.remove(employee);
+        }
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+}
