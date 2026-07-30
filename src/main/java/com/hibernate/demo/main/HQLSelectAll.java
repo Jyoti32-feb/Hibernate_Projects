a65693a3a0d2e2df00fe13b362e6ff6d31925f50
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-30 18:39:38 +0530

    data fetching using list ,uniqueresult and getsingleresult

--- src/main/java/com/hibernate/demo/main/HQLSelectAll.java
+++ src/main/java/com/hibernate/demo/main/HQLSelectAll.java
@@ -16,11 +16,41 @@ public class HQLSelectAll {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
 
-        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+        Query<Employee> query = session.createQuery("from Employee where salary > :salary And name=:name", Employee.class);
+        query.setParameter("salary", 50000);
+        query.setParameter("name", "Jyoti");
         List<Employee> employees = query.list();
         for (Employee employee : employees) {
             System.out.println(employee);
         }
+        transaction.commit();
+        session.close();
+
+        Session session1 = sessionFactory.openSession();
+        Transaction transaction1 = session1.beginTransaction();
+
+        Query<Employee> query1 = session1.createQuery("from Employee where id =:id", Employee.class);
+        query.setParameter("id", 1);
+
+        Employee employee = query1.uniqueResult();
+
+        System.out.println(employee);
+
+        transaction1.commit();
+        session1.close();
+
+        Session session2 = sessionFactory.openSession();
+        Transaction transaction2 = session2.beginTransaction();
+
+        Query<Employee> query2 = session2.createQuery("from Employee where id =:id", Employee.class);
+        query.setParameter("id", 10);
+
+        Employee employee1 = query.getSingleResult();
+
+        System.out.println(employee1);
+
+        transaction2.commit();
+        session2.close();
     }
 
 }
